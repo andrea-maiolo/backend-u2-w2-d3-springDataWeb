db0421c536dce939bf3d54e11f0c8771ccf84c8e
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-07-23 14:14:33 +0200

    first commit

---
+++ src/test/java/am/demo/AndreamApplicationTests.java
@@ -0,0 +1,13 @@
+package am.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
